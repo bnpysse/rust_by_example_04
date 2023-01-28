496fa35853132caf69a6fe511910f188b4cbb09e
Woodman <bnpysse@aliyun.com>
2023-01-28 18:36:20 +0800

    Processing ToString & FromStr

--- src/main.rs
+++ src/main.rs
@@ -158,8 +158,114 @@ fn main() {
     println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
     println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
     
+    // fun(&foo) 用 传引用(pass by reference)的方式把变量传给函数，而非传值
+    // (pass by value)，写法是 fun(foo))
+    // std::mem::size_of_val 是一个函数，这里是完整路径调用，代码可以分成一些
+    // 叫做模块(module)的逻辑单元。在本例中， size_of_val是在 mem 模块中定义的，
+    // 而 mem 模块又是在 std crate 中定义的。
 
+    // 5.3.类型推断
+    println!("\n\n=====5.3.类型推断=====");
+    // Rust 的类型推断引擎是很聪明的，它不只是在初始化时年看右值 (r-value) 的类型而已，
+    // 它还会考察变量之后会怎样使用，借此推断类型。
+    
+    // 因为有类型说明，编译器知道 elem 的类型是 u8。
+    let elem = 5_u8;
+    // 创建一个空向量(vector,即不定长的，可以增长的数组)
+    let mut vec = Vec::new();
+    // 现在编译器还不知道 vec 的具体类型，只知道它是某种东西构成的
+    // 向量 (Vec<_>)
+    // 在向量中插入 elem
+    vec.push(elem);
+    // 哈哈，现在编译器就知道 vec 是 u8 的向量了 (`Vec<u8>`>)。
+    println!("the vec is: {:?}, the size of vec is: {}", vec, std::mem::size_of_val(&vec));
 
+    // 5.4.别名
+    println!("\n\n=====5.4.别名=====");
+    // 可以用 type 语句给已有的类型取个新的名字。
+    // 类型的名字必须遵循驼峰命名法，否则编译器将给出警告
+    // 原生类型除外，比如：usize、f32等等
+    // `NanoSecond` 是 `u64` 的新名字
+    type NanoSecond = u64;
+    type Inch = u64;
+
+    // 通过这个属性屏蔽警告
+    #[allow(non_camel_case_types)]
+    type u64_t = u64;
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+    println!("{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches);
+    // 别名的主要用途是避免写出冗长的模板化代码(boilerplate code)
+    // 如 IoResult<T> 是 Result<T, IoError> 类型的别名
+
+    // 6.类型转换
+    println!("\n\n=====6.类型转换=====");
+    // Rust 使用 trait 解决类型之间的转换问题。
+    // 最一般的转换会用到 From 和 Into 两个trait
+    // 不过，即便常见的情况也可能会用到特别的 trait ，尤其是 String 转换到别的类型
+    // 以及把别的类型转换到 String 时
+
+    // 6.1.From和Into
+    println!("\n\n=====6.1.From和Into=====");
+    // From Into两个 trait 是内部相联的，实际上这是它们实现的一部分。
+    // 如果我们能够从类型 A 得到类型 B ，那么很容易相信我们也能把类型B转换为类型A
+    // From trait允许一种类型定义“怎么根据另一种类型生成自己”，因此它提供了一种
+    // 类型转换的简单机制，在标准库中有无数 From 的实现，规定原生类型及其他常见类型的转换
+    // 比如，可以 很容易地把 str 转换成 String
+    // 为自己的类型定义转换机制
+    use std::convert::From;
+    #[derive(Debug)]
+    struct Number {
+        value: i32,
+    }
+    impl From<i32> for Number {
+        fn from(item: i32) -> Self {
+            Number { value: item}
+        }
+    }
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    // Into trait就是把 From trait 倒过来而已
+    // 也就是说，你为你的类型实现了 From，那么同时你也就免费获得了 Into
+    // 使用 Into trait 通常要求指明要转换到的类型
+    // 因为编译器大多数时候不断推断它
+    // 我们现在可以直接使用 Into
+    let int = 5;
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+    
+    // 6.2.TryFrom和TryInto
+    println!("\n\n=====6.2.TryFrom和TryInto=====");
+    // 类似于 From 和 Into，TryFrom和TryInto是类型转换的通用trait
+    // 不同于 From/Into 的是，TryFrom 和 TryInto trait用于易出错的
+    // 转换，也正因为如此，其返回值是 Result 型。
+    use std::convert::TryFrom;
+    use std::convert::TryInto;
+    #[derive(Debug, PartialEq)]
+    struct EvenNumber(i32);
+    impl TryFrom<i32> for EvenNumber {
+        type Error = ();
+        fn try_from(value: i32) -> Result<Self, Self::Error> {
+            if value % 2 == 0 {
+                Ok(EvenNumber(value))
+            } else {
+                Err(())
+            }
+        }
+    }
+    // TryFrom
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+    
+    // TryInto
+    let result: Result<EvenNumber, ()> = 8_i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5_i32.try_into();
+    assert_eq!(result, Err(()));
     
     
     
