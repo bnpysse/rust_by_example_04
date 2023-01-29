c6955da4ae7387e19c53e0259b8cd47e9874559c
Woodman <bnpysse@aliyun.com>
2023-01-29 18:32:35 +0800

    Learning Closure

--- src/main.rs
+++ src/main.rs
@@ -876,5 +876,37 @@ fn main() {
     
     // 9.2.闭包
     println!("\n\n=====9.2.闭包=====");
-    
+    // Rust 中的闭包(closure)，也称之为lambda表达式或者 lambda，是一类
+    // 能够捕获周围作用域中变量的函数。例如，一个可以捕获 x 变量的闭包如下：
+    //          |val| val + x
+    // 其语法和能力使其在临时(on the fly)使用相当方便。
+    // 调用一个闭包和调用一个函数完全相同，不过调用闭包时，
+    // 输入和返回类型两者都可以自动推导，而输入变量名必须指明
+    // 其他的特点包括：
+    //      声明时使用 || 替代 () 将输入参数括起来。
+    //      函数体定界符 ({}) 对于单个表达式是可选的，其他情况必须加上
+    //      有能力捕获外部环境的变量
+    // 
+    // 通过闭包和函数分别实现自增，这个是函数实现
+    fn function(i: i32) -> { i + 1 }
+    // 闭包匿名的，这里我们将其绑定到引用。
+    // 类型标注和函数的一样，不过类型标准和使用 '{}' 来围住函数体是可选的
+    // 这些匿名函数(nameless function)被赋值赋值给合适的命名的变量
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred = |i| i + 1;
+    // 闭包表达式产生的类型就是 闭包类型，不属于引用类型，
+    // 确实无法对上面的两个变量进行 解引用。
+    let i = 1;
+    // 调用函数和闭包
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    // 没有参数的闭包，返回一个 'i32' 类型
+    // 返回类型是自动推导的
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+
+
 }
