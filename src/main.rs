59aef48d73222c82507c429b562cf877d54eeaf8
bnpysse <bnpysse@aliyun.com>
2023-01-30 12:03:47 +0800

    Starting 9.2.2.作为输入参数

--- src/main.rs
+++ src/main.rs
@@ -910,7 +910,7 @@ fn main() {
     // 9.2.1.捕获
     println!("\n\n=====9.2.1.捕获=====");
     // 闭包本质上很灵活，能做功能要求的事情，使闭包在没有类型标注的情况下运行。
-    // 这使得捕获 (capture) 能够灵活地适应用例，既可移动 (move) ，又可借用 (borrow) 
+    // 这使得捕获 (capture) 能够灵活地适应用例，既可移动 (move) ，又可借用 (borrow)
     // 闭包可以通过以下方式捕获变量：
     //      通过引用： &T
     //      通过可变引用： &mut T
@@ -932,9 +932,48 @@ fn main() {
     let _color_moved = color;
 
     let mut count = 0;
-    //    ta 
-    
-    
+    // 这个闭包使 'count' 值增加，要做到这点，它需要得到 '&mut count' 或者 'count' 本身，
+    // 但 '&mut count' 的要求没那么严格，所以我们采取这种方式。
+    // 该闭包立即借用 'count'
+    // 'inc' 前面需要加上'mut'，因为闭包里存储着一个 '&mut' 变量，调用闭包时，该变量的变化
+    // 就意味着闭包内部发生了变化。因此闭包需要是可变的。
+    let mut inc = || {
+        count += 1;
+        println!("'count' is: {}", count);
+    };
+    // 使用可变借用调用闭包
+    inc();
+
+    // 因为之后调用闭包，所以仍然可变借用 'count'。试图重新借用将导致错误
+    // let _reborrow = &count;
+    inc();
+
+    // 闭包不再借用 '&mut count' ，因此可以正确地重新借用
+    let _count_reborrowed = &mut count;
+
+    // 不可复制类型 (non-copy type)
+    let movable = Box::new(3);
+    // 'mem::drop' 要求 'T' 类型本身，所以闭包将会捕获变量的值，这种情况下，
+    // 可复制类型将会提制给闭包，从而原始值不受影响。
+    // 不可复制类型必须移动 (move) 到闭包中，因而 'movable' 变量在这里立即
+    // 移动到了闭包中。
+    let consume = || {
+        println!("'movable': {:?}", movable);
+        mem::drop(movable);
+    };
+    // 'consume' 消耗了该变量，所以该闭包只能调用一次。
+    consume();
+    // 在竖线 | 之前使用 move 会强制闭包取得被捕获变量的所有权。
+    let haystack = vec![1, 2, 3];
+    let contains = move |needle| haystack.contains(needle);
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    // 9.2.2.作为输入参数
+    println!("\n\n=====9.2.2.作为输入参数=====");
+
+
+
 
 
 }
